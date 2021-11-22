5d2fcd06bd9ba1a1180229b31617859469165d14
Victor <80902748+leonidasfr65@users.noreply.github.com>
2021-11-22 22:48:42 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://dbUser:' + encodeURIComponent('F4hgFhn6yW7uPFA') + '@cluster0.tt1gk.mongodb.net/elevatedb?retryWrites=true';
+dbPassword = 'mongodb+srv://dbUser:' + encodeURIComponent('F4hgFhn6yW7uPFA') + '@cluster0.5njwh.mongodb.net/elevatedb?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword
